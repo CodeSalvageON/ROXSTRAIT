5d3e5c7645fb6921f53ddbcdb4568e6720394e0f
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-07-18 01:15:11 +0000

    add more to public sector

--- public/static/btnfunc.js
+++ public/static/btnfunc.js
@@ -1,6 +1,12 @@
 const respubBtn = document.getElementById("respub");
+const rRespub = document.getElementById("rRespub");
 
 respubBtn.onclick = function () {
   welcomeSec.style.display = "none";
   respubSec.style.display = "block";
+}
+
+rRespub.onclick = function () {
+  respubSec.style.display = "none";
+  welcomeSec.style.display = "block";
 }
