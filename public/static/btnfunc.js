277ba59c953adb75ee272f32dd647ceb9b89cdf6
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-07-19 13:56:49 +0000

    ooga booga

--- public/static/btnfunc.js
+++ public/static/btnfunc.js
@@ -1,6 +1,9 @@
 const respubBtn = document.getElementById("respub");
 const rRespub = document.getElementById("rRespub");
 
+const projectsBtn = document.getElementById("projects");
+const rProjects = document.getElementById("rProjects");
+
 respubBtn.onclick = function () {
   welcomeSec.style.display = "none";
   respubSec.style.display = "block";
@@ -8,5 +11,15 @@ respubBtn.onclick = function () {
 
 rRespub.onclick = function () {
   respubSec.style.display = "none";
+  welcomeSec.style.display = "block";
+}
+
+projectsBtn.onclick = function () {
+  welcomeSec.style.display = "none";
+  projectsSec.style.display = "block";
+}
+
+rProjects.onclick = function () {
+  projectsSec.style.display = "none";
   welcomeSec.style.display = "block";
 }
--- public/static/domvar.js
+++ public/static/domvar.js
@@ -1,3 +1,4 @@
 const display = document.getElementById("display");
 const welcomeSec = document.getElementById("welcome");
 const respubSec = document.getElementById("respub-sec");
+const projectsSec = document.getElementById("projects-sec");
